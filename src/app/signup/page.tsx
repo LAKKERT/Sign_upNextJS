e779ccff783e80ed561ff26ae5d1ff1ad7d5956c
AHRI <lakkert2002@yandex.ru>
2024-02-25 18:08:53 +0500

    new file:   src/app/api/users/login/route.ts 	new file:   src/app/api/users/signup/route.ts 	new file:   src/app/login/page.tsx 	new file:   src/app/signup/page.tsx

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
+export default function SignupPage() {
+     
+
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-white text-2xl">Login</h1>
+        </div>
+    );
+}
